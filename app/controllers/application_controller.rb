a75fcd84867dd151df98bb0e8ee34f4b694d18a1
Lucas Willett <me@ltw.io>
2015-04-06 14:32:48 -0500

    add sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,16 @@ class ApplicationController < ActionController::Base
   def current_user
     @_current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
+  helper_method :current_user
 
   def logged_in?
     !!current_user
   end
+  helper_method :logged_in?
 
   def authenticate
     unless logged_in?
-      redirect_to new_sessions_path, alert: "Please log in."
+      redirect_to new_session_path, alert: "Please log in."
       return false
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
+  root to: 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
